6c09a715c7b6b50da8b4afbe82377fc03189f61d
Dmitry <gavrilovdv1975@yandex.ru>
2024-06-25 18:26:51 +0300

    Revert "Merge branch 'new-test'"
    This reverts commit f017505391b6657cef5466bea892f5200af1767f, reversing
    changes made to 131d640ac8232c81f85ca113475dff553f564b6d.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # Урок OG04.Использование GitHub для совместной разработки и расширенные возможности Git
 
+<<<<<<< HEAD
+a = 7 # создание переменной
+b = 9
+=======
 for i in range(8):
     print("слово")
 
 a, b = 9, 8
+>>>>>>> parent of 5aabeb3 (Update main.py)
 
 if a > b:
     print("а больше чем b")
@@ -11,5 +16,9 @@ elif b > a:
     print("b больше чем a")
 else:
     print("a равно b")
+<<<<<<< HEAD
 print("нам это не нужно")
-print("я конфликтую")
+
+=======
+print("нам это не нужно")
+>>>>>>> parent of 5aabeb3 (Update main.py)
