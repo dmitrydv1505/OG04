b6e60b5c8c9702d1ebd4440d1c37be8202daceb7
Dmitry <gavrilovdv1975@yandex.ru>
2024-06-25 18:50:29 +0300

    merge main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 # Урок OG04.Использование GitHub для совместной разработки и расширенные возможности Git
 
-<<<<<<< HEAD
-a = 7 # создание переменной
+a = 5 # создание переменной
 b = 9
-=======
 for i in range(8):
-    print("слово")
-
-a, b = 9, 8
->>>>>>> parent of 5aabeb3 (Update main.py)
 
 if a > b:
     print("а больше чем b")
@@ -16,9 +10,3 @@ elif b > a:
     print("b больше чем a")
 else:
     print("a равно b")
-<<<<<<< HEAD
-print("нам это не нужно")
-
-=======
-print("нам это не нужно")
->>>>>>> parent of 5aabeb3 (Update main.py)
